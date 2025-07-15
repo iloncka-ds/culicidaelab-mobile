a71247aefdbd0120b15b41b0626f01d2a98cd21c
Ilona Kovaleva <iloncka.ds@gmail.com>
2025-07-15 16:03:58 +0300

    Add lint options for build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,6 +19,12 @@ android {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
+    lintOptions {
+        isAbortOnError = false
+        isCheckReleaseBuilds = false
+        disable.add("UnsafeOperations")
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.culicidaelab"
